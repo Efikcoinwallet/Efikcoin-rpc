e8acc2652bd541e71f13d2bf317d956c41832512
Wallet <efikcoin@gmail.com>
2026-08-22 15:02:50 +0200

    Create chain-20488.js

---
+++ chain-20488.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "Efikcoin Mainnet",
+  chain: "Efikcoin",
+  icon: "efikcoin",
+  rpc: ["https://rpc.efikcoin.com/rpc"],
+  faucets: [],
+  nativeCurrency: {
+    name: "Efikcoin",
+    symbol: "EFC",
+    decimals: 18
+  },
+  infoURL: "https://efikcoin.com",
+  shortName: "efik",
+  chainId: 20488,
+  networkId: 20488,
+  explorers: [{
+    name: "Efikcoin Explorer",
+    url: "https://explorer.efikcoin.com",
+    standard: "EIP3091"
+  }],
+  status: "active"
+}
